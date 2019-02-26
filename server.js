f337cacfd4d2e533201bb7183a13292c2700f267
anderson <anderson771218@gmail.com>
2019-02-27 07:22:10 +0800

    Add /profile and /image API

--- server.js
+++ server.js
@@ -29,6 +29,37 @@ app.get('/', (req, res) => {
     res.send(database.users);
 });
 
+
+//we should return user immediately after matching 
+app.get('/profile/:id', (req, res) => {
+    const { id } = req.params;
+    let found = false;
+    database.users.forEach(user => {
+        if(user.id === id){
+            found = true;
+            return res.json(user);
+        }
+    })
+    if(!found){
+        res.status(400).json('not found');
+    }
+});
+
+app.put('/image', (req, res) => {
+    const { id } = req.body;
+    let found = false;
+    database.users.forEach(user => {
+        if(user.id === id){
+            found = true;
+            user.entries++;
+            return res.json(user.entries);
+        }
+    })
+    if(!found){
+        res.status(400).json('not found');
+    }
+});
+
 app.post('/signin', (req, res) => {
     if (req.body.email === database.users[0].email && req.body.password === database.users[0].password){
         res.json('success');
@@ -49,6 +80,7 @@ app.post('/register', (req, res) => {
     });
     res.json(database.users[database.users.length - 1]);
 });
+
 
 
 app.listen(3000, () => {
