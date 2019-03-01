373044fe3f3a2afa3c42b01bddba3f45c202d30b
anderson <anderson771218@gmail.com>
2019-03-02 02:37:02 +0800

    Add new endpoint(handleApiCall)

--- controllers/image.js
+++ controllers/image.js
@@ -1,3 +1,16 @@
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+    apiKey: 'MY KEY'
+   });
+
+const handleApiCall = (req, res) => {
+    app.models
+      .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+      .then(data => res.json(data))
+      .catch(err => res.status(400).json('unable to work with API') );
+}
+
 const handleImage = (req, res, db) => {
     const { id } = req.body;
     db('users').where('id', '=', id)
@@ -9,4 +22,4 @@ const handleImage = (req, res, db) => {
     .catch(err => res.status(400).json('unable to get entries'))
 }
 
-module.exports = { handleImage };
+module.exports = { handleImage, handleApiCall };
